b5fd5cfc28fb86943f7d407cdec279b92603a6f7
Kalaiarasi07 <66557110+Kalaiarasi07@users.noreply.github.com>
2022-03-01 10:36:01 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,67 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 100
+#define PORT 8080
+#define SA struct sockaddr
+void clientChat(int fd)
+{
+	char buf[BUF_SIZE];
+	int n;
+	while(1) {
+		bzero(buf, sizeof(buf));
+		printf("Client says : ");
+		n = 0;
+		while ((buf[n++] = getchar()) != '\n')
+			;
+		write(fd, buf, sizeof(buf));
+		bzero(buf, sizeof(buf));
+		read(fd, buf, sizeof(buf));
+		printf("Server says : %s", buf);
+		if ((strncmp(buf, "exit", 4)) == 0) {
+			printf("Client Exits...\n");
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int soc;
+
+	// socket creation
+	soc = socket(AF_INET, SOCK_STREAM, 0);
+	if (soc==-1) {
+		printf("socket creation at client failed!\n");
+		exit(0);
+	}
+	else
+		printf("Socket creation at client  successful!\n");
+	struct sockaddr_in servAddress;
+	bzero(&servAddress, sizeof(servAddress));
+
+	// assign IP, PORT
+	servAddress.sin_family = AF_INET;
+	servAddress.sin_addr.s_addr = inet_addr("127.0.0.1");//Converts string to inet address
+	servAddress.sin_port = htons(PORT);
+
+	// connecting client socket to server socket
+	int con = connect(soc, (SA*)&servAddress, sizeof(servAddress));
+	if(con!=0)	{
+		printf("connection with server failed\n");
+		exit(0);
+	}
+	else
+		printf("connection with server successful\n");
+
+	// function for chat
+	clientChat(soc);
+
+	// close the socket
+	close(soc);
+}
+
